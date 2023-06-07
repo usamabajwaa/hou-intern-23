1c6b62a45ddd1a83ae4c951f365c38c4c94b38dd
gabrielmayfield <gabriel.mayfield@bjss.com>
2023-06-07 16:04:24 -0500

    Updated class by removing unused code

--- weather/Weather.Viz.py
+++ weather/Weather.Viz.py
@@ -10,24 +10,3 @@ class Weather_Viz:
         self.API_token = API_token
         # Fetch the data and store it in the DF propetry
         self.data_frame = self.get_data()
-
-    def get_data(self): 
-        # Define the headers for API request
-        headers = {
-            'Authorization' : f'Bearer {self.API_token}',
-        }
-
-        # Send GET request to the API_url with the headers
-        response = requests.get(self.API_url, headers=headers)
-
-        # Check that the GET request was successful. If not, raise an error. 
-        if response.status_code != 200: 
-            raise Exception(f"GET request to {self.API_URL} failed with status code {response.status_code}")
-        
-        # Convert the JSON response to a dictionary
-        data = response.json()
-
-        # Convert the data dictionary to a DF and return it 
-        data_frame = pd.DataFrame(data)
-
-        return data_frame
