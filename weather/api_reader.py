23371b5dd943e93336d1cd29bb56589a509371fe
Brandon Eigenschenk <brandon.eigenschenk@bjss.com>
2023-06-08 12:57:59 -0500

    API reader was updated to include dataframe and return the value. Also set up to read in a API_Url and API_token

--- weather/api_reader.py
+++ weather/api_reader.py
@@ -1,4 +1,5 @@
 import requests
+import pandas as pd
 from datetime import datetime, timedelta
 
 def getYearData():
@@ -6,10 +7,13 @@ def getYearData():
     """
     reads in the API request and uses f-strings to adjust the start date and end date for collecting necessary data.
 
-    the output should be both a date (format YYYY-MM-DD) and the daily avg temp in Fahrenheit
+    the output should be a pandas dataframe containing both the date (format YYYY-MM-DD) and the daily avg temp in Fahrenheit
 
     dateStart and dateEnd are adjusted after each execution of the for loop due and are re-inserted into the api key parameters
     """
+    
+    API_url = 'http://api.weatherapi.com/v1/history.json'
+    API_token = 'b5d1d68681064ffcb53173100230706'
     
     #Populates the current day and sets a datestart 365 days in the past and sets an end date 30 days after due to api 
     #restrictions set to gathering only ~30 days of data at a time 
@@ -17,18 +21,27 @@ def getYearData():
     dateStart = currDate - timedelta(days=365)
     dateEnd = dateStart + timedelta(days=30)
     
+    #init pandas dataframe
+    parameters = ['date', 'avgtemp_f']
+    df = pd.DataFrame(columns=parameters)
+
     for i in range(0, 13):
-        result = requests.get(f'http://api.weatherapi.com/v1/history.json?key=b5d1d68681064ffcb53173100230706&q=Houston&dt={dateStart}&end_dt={dateEnd}')
+        result = requests.get(f'{API_url}?key={API_token}&q=Houston&dt={dateStart}&end_dt={dateEnd}')
         result_dict = dict(result.json())
         if result_dict == None: break
 
+        #For loop checks the dictionary for the necessary values and stores them into a new_row dataframe, it is then
+        #transposed and concat'd with the pre-existing dataframe
         for i in range(len(result_dict['forecast']['forecastday'])):
-            print(f"Date: {result_dict['forecast']['forecastday'][i]['date']}")
-            print(f"Temp: {result_dict['forecast']['forecastday'][i]['day']['avgtemp_f']}°F\n")
+            date = result_dict['forecast']['forecastday'][i]['date']
+            avgTempF = result_dict['forecast']['forecastday'][i]['day']['avgtemp_f']
             
+            new_row = pd.DataFrame([date, avgTempF], index=parameters).T
+            df = pd.concat([df,new_row], ignore_index=True)
+
         dateStart = dateEnd + timedelta(days=1)
         dateEnd = dateStart + timedelta(days=30)
 
         if dateEnd >= datetime.date(datetime.now()): dateEnd = datetime.date(datetime.now())
         
-getYearData()
+    return df
