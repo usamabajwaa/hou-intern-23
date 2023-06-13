5c8fbb1563e49eaf0605435f0fb8221390e84748
Brandon Eigenschenk <brandon.eigenschenk@bjss.com>
2023-06-13 10:10:28 -0500

    First pass for historical yearly visualization data

--- weather/Weather.Viz.py
+++ weather/Weather.Viz.py
@@ -1,3 +1,4 @@
+from matplotlib import  dates, pyplot as plt
 import pandas as pd
 from api_reader import getYearData
 
@@ -31,7 +32,6 @@ class WeatherViz:
         
         # convert 'date'column to datetime format
         df['date'] = pd.to_datetime(df['date'])
-
         # Extract month and avg temp data
         df['month'] = df['date'].dt.month
         monthly_df = df.groupby(['month'])['avgtemp_f'].mean().reset_index()
@@ -40,3 +40,27 @@ class WeatherViz:
         monthly_df.rename(columns={'avgtemp_f': 'avg_temp'}, inplace=True)
 
         return monthly_df
+    
+def yearVizData(yearData):
+
+    #Sets up and autoscales the window to a larger size for readbabilit
+    fig = plt.figure(figsize=(20, 7), dpi=100)
+    ax = fig.subplots()
+    
+    ax.plot(yearData.data_frame['date'], yearData.data_frame['avgtemp_f'])
+    
+    #Sets X-axis increments
+    ax.xaxis.set_major_locator(dates.DayLocator(interval=14)) #Sets major tick marks on x-axis to once every month
+    ax.xaxis.set_minor_locator(dates.DayLocator()) #Sets minor tick marks on x-axis to once every week
+        
+    ax.set(xlabel = 'Date', ylabel = 'Avg Temp (°F)', title = 'Historical Data Showing Avg Temp (°F) for Past 365 Days')
+
+    #Displays grid and formats x-axis test
+    plt.grid(which = 'both', axis='x')
+    plt.grid(which = 'both', axis='y')
+    plt.gcf().autofmt_xdate()
+    plt.show()
+
+test = WeatherViz('http://api.weatherapi.com/v1/history.json')
+test.yearData(test.API_url)
+testGraph = yearVizData(test)
