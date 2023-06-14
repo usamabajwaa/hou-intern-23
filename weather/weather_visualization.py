66290864597522b5731b19c2ef4450954e736e1e
David Reedy <drreedy91@gmail.com>
2023-06-14 11:09:22 -0500

    updated pull request

--- weather/weather_visualization.py
+++ weather/weather_visualization.py
@@ -64,6 +64,7 @@ class WeatherViz:
         plt.grid(True)
         # Finally, display the grid
         plt.show()
+
     def calculateMonthlyAverages(self, df):
         """
         Calculates monthly average weather given datafram of last 365 days
@@ -85,22 +86,56 @@ class WeatherViz:
 
         return monthly_df
     
-def yearVizData(yearData):
+    def yearVizData(yearData):
 
-    #Sets up and autoscales the window to a larger size for readbabilit
-    fig = plt.figure(figsize=(20, 7), dpi=100)
-    ax = fig.subplots()
+        #Sets up and autoscales the window to a larger size for readbabilit
+        fig = plt.figure(figsize=(20, 7), dpi=100)
+        ax = fig.subplots()
     
-    ax.plot(yearData.data_frame['date'], yearData.data_frame['avgtemp_f'])
+        ax.plot(yearData.data_frame['date'], yearData.data_frame['avgtemp_f'])
     
-    #Sets X-axis increments
-    ax.xaxis.set_major_locator(dates.DayLocator(interval=14)) #Sets major tick marks on x-axis to once every month
-    ax.xaxis.set_minor_locator(dates.DayLocator()) #Sets minor tick marks on x-axis to once every week
+        #Sets X-axis increments
+        ax.xaxis.set_major_locator(dates.DayLocator(interval=14)) #Sets major tick marks on x-axis to once every month
+        ax.xaxis.set_minor_locator(dates.DayLocator()) #Sets minor tick marks on x-axis to once every week
         
-    ax.set(xlabel = 'Date', ylabel = 'Avg Temp (°F)', title = 'Historical Data Showing Avg Temp (°F) for Past 365 Days')
+        ax.set(xlabel = 'Date', ylabel = 'Avg Temp (°F)', title = 'Historical Data Showing Avg Temp (°F) for Past 365 Days')
 
-    #Displays grid and formats x-axis test
-    plt.grid(which = 'both', axis='x')
-    plt.grid(which = 'both', axis='y')
-    plt.gcf().autofmt_xdate()
-    plt.show()
+        #Displays grid and formats x-axis test
+        plt.grid(which = 'both', axis='x')
+        plt.grid(which = 'both', axis='y')
+        plt.gcf().autofmt_xdate()
+        plt.show()
+
+        #plots average monthly temp
+
+    def plotAverageMonthlyTemperature(monthly_df):
+        """
+        Plots the average monthly temperature for the last year using a line chart.
+
+        Args:
+            monthly_df: A pandas DataFrame containing the average monthly temperatures.
+
+        Returns:
+            None (displays the line chart using matplotlib).
+        """
+        # Extract month and average temperature data
+        months = monthly_df['month']
+        avg_temp = monthly_df['avg_temp']
+    
+        # Set up the figure and axes
+        fig, ax = plt.subplots()
+    
+        # Plot the line chart
+        ax.plot(months, avg_temp, marker='o', linestyle='-', color='blue')
+    
+        # Set labels and title
+        ax.set_xlabel('Month')
+        ax.set_ylabel('Average Temperature')
+        ax.set_title('Average Monthly Temperature for the Last Year')
+    
+        # Set x-axis ticks and labels
+        ax.set_xticks(months)
+        ax.set_xticklabels(['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'])
+    
+        # Display the chart
+        plt.show()
