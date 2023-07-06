b52befcc8a34f6289453fc9ba75a76f4eecd41f1
David Reedy <drreedy91@gmail.com>
2023-07-06 15:22:27 -0500

    updated file name transfomers class

--- weather/main.py
+++ weather/main.py
@@ -32,11 +32,12 @@ if __name__== "__main__":
     if verbose:
         print("Visualzing weather data")
 
+        
     weather_viz = WeatherViz('http://api.weatherapi.com/v1/history.json')
     weather_viz.yearData('http://api.weatherapi.com/v1/history.json')
     weather_viz.monthly_df = weather_viz.calculateMonthlyAverages(weather_viz.data_frame)
     weather_viz.yearVizData(weather_viz.data_frame)
     weather_viz.plotLast30Days()
     weather_viz.plotAverageMonthlyTemperature(weather_viz.monthly_df)
-    print(weather_viz.data_frame)
+
     
